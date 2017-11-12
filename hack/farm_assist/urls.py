06db659165110ee4d2310c90e3e87f7e6c60450f
krutik710 <menkudlekrutik@gmail.com>
2017-11-12 16:49:47 +0530

    skksks

--- hack/farm_assist/urls.py
+++ hack/farm_assist/urls.py
@@ -4,6 +4,7 @@ from farm_assist import views
 
 urlpatterns = [
     url(r'^login/', views.login_site),
+    url(r'^logout/', views.logout_site),
     url(r'^register/', views.register),
     url(r'^farmer_homepage/', views.farmer_homepage),
     url(r'^seeds/', views.seeds),
@@ -11,5 +12,8 @@ urlpatterns = [
     url(r'^discuss/', views.discuss),
     url(r'^subs/',views.subs),
     url(r'^disease/', views.disease),
+
+
+    url(r'^simulation/', views.simulation),
 
 ]
